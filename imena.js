db8861cd6665caef419e120f3ecbff26a4d02a46
tedige <114273306+tedige@users.noreply.github.com>
2022-12-17 04:04:46 +0600

    Update imena.js

--- imena.js
+++ imena.js
@@ -42,6 +42,6 @@ function sendData() {
         error = true;
     }
     if (error === false) {
-        window.location.href = "/second/index.html";
+        window.location.href = "index.html";
     }
 }
